7a4d009ddfffaf083280e747094a4fddaad272bc
hamish2014 <antoine.dymond@gmail.com>
2014-12-02 16:48:55 +0200

    generate parts lists function added

--- InitGui.py
+++ InitGui.py
@@ -10,13 +10,13 @@ class DimensioningWorkbench (Workbench):
         if DEBUG:
             from dimensioning import __dir__, debugPrint
             import crudeDebugger, os
-        for module in ['linearDimension', 'deleteDimension', 'circularDimension', 'addText', 'escapeDimensioning', 'angularDimension']:
+        for module in ['linearDimension', 'deleteDimension', 'circularDimension', 'addText', 'escapeDimensioning', 'angularDimension', 'partsList']:
             if not DEBUG:
                 importlib.import_module( module )
             else:
                 crudeDebugger.printingDebugging( os.path.join(__dir__, module + '.py') )
                 importlib.import_module(  module + '_crudeDebugging')
-        commandslist = ['linearDimension', 'circularDimension', 'angularDimension', 'addTextDimensioning', 'deleteDimension', 'escapeDimensioning']
+        commandslist = ['linearDimension', 'circularDimension', 'angularDimension', 'addTextDimensioning', 'deleteDimension', 'escapeDimensioning', 'addPartsList']
         self.appendToolbar('Drawing Dimensioning', commandslist)
 
 Gui.addWorkbench(DimensioningWorkbench())
