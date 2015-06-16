3a3d7f0dc5a1166e87dacda168d182f84c948fb0
hamish <antoine.dymond@gmail.com>
2015-06-16 22:14:19 +0200

    between parallels creation option for center line added.

--- centerLines.py
+++ centerLines.py
@@ -2,6 +2,7 @@
 from dimensioning import *
 import selectionOverlay, previewDimension
 from dimensionSvgConstructor import *
+from linearDimension import linearDimension_parallels_hide_non_parallel
 
 d = DimensioningProcessTracker()
 
@@ -64,14 +65,27 @@ def centerLine_clickHandler( x, y ):
         return 'createDimension:%s' % findUnusedObjectName('centerLines')
 
 def selectFun(  event, referer, elementXML, elementParms, elementViewObject ):
-    x,y = elementParms['x'], elementParms['y']
-    d.args = [[x, y]]
-    d.stage = 1
-    d.dimScale = elementXML.rootNode().scaling()
-    debugPrint(3, 'center selected at x=%3.1f y=%3.1f scale %3.1f' % (x,y, d.dimScale))
-    selectionOverlay.hideSelectionGraphicsItems()
-    previewDimension.initializePreview( d, centerLine_preview , centerLine_clickHandler)
-
+    if not isinstance(referer,selectionOverlay.LineSelectionGraphicsItem):
+        x,y = elementParms['x'], elementParms['y']
+        d.args = [[x, y]]
+        d.stage = 1
+        d.dimScale = elementXML.rootNode().scaling()
+        debugPrint(3, 'center selected at x=%3.1f y=%3.1f scale %3.1f' % (x,y, d.dimScale))
+        selectionOverlay.hideSelectionGraphicsItems()
+        previewDimension.initializePreview( d, centerLine_preview , centerLine_clickHandler)
+    else:
+        if d.stage == 0:
+            d.line1 = [ elementParms[k] for k in [ 'x1', 'y1', 'x2', 'y2' ] ]
+            d.stage = 1
+            linearDimension_parallels_hide_non_parallel( elementParms, elementViewObject)
+        else:
+            x1, y1, x2, y2 = d.line1
+            x3, y3, x4, y4 = [ elementParms[k] for k in [ 'x1', 'y1', 'x2', 'y2' ] ]
+            d.args = [[0.25*(x1 + x2 + x3 + x4), 0.25*(y1 + y2 + y3 + y4)]]
+            d.stage = 1
+            d.dimScale = elementXML.rootNode().scaling()
+            selectionOverlay.hideSelectionGraphicsItems()
+            previewDimension.initializePreview( d, centerLine_preview , centerLine_clickHandler)
 
 
 class CenterLines:
@@ -102,6 +116,11 @@ class CenterLines:
             } 
 FreeCADGui.addCommand('dd_centerLines', CenterLines())
 
+line_maskPen =      QtGui.QPen( QtGui.QColor(0,255,0,100) )
+line_maskPen.setWidth(2.0)
+line_maskHoverPen = QtGui.QPen( QtGui.QColor(0,255,0,255) )
+line_maskHoverPen.setWidth(2.0)
+line_maskBrush = QtGui.QBrush() #clear
 
 class CenterLine(CenterLines):
     def Activated(self):
@@ -118,11 +137,22 @@ class CenterLine(CenterLines):
             selectFun,
             transform = V.transform,
             sceneToAddTo = V.graphicsScene, 
-            doPoints=True, doMidPoints=True, 
+            doPoints=True, doMidPoints=True,
             pointWid=1.0,
             maskPen=maskPen, 
             maskHoverPen=maskHoverPen, 
             maskBrush = maskBrush
+            )
+        selectionOverlay.generateSelectionGraphicsItems( 
+            [obj for obj in V.page.Group  if not obj.Name.startswith('dim') and not obj.Name.startswith('center')],
+            clearPreviousSelectionItems = False,
+            doLines=True, 
+            onClickFun=selectFun,
+            transform = V.transform,
+            sceneToAddTo = V.graphicsScene, 
+            maskPen= line_maskPen, 
+            maskHoverPen= line_maskHoverPen, 
+            maskBrush = line_maskBrush
             )
         selectionOverlay.addProxyRectToRescaleGraphicsSelectionItems( V.graphicsScene, V.graphicsView, V.width, V.height)
 
